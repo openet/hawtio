6a83cd98975ba3d37380df5e300c844741860bff
astulka <artur.stulka@openet.com>
2014-10-10 17:20:56 +0100

    Openet branch rebased to hawtio 1.4.21

--- hawtio-web/src/main/webapp/vendor.js
+++ hawtio-web/src/main/webapp/vendor.js
@@ -6,6 +6,6 @@ localStorage['showWelcomePage'] = false;
 // delete all tabs from default perspective
 Perspective.metadata.container.topLevelTabs.excludes = [{}];
 // add Camel and Connect to the default perspective
-Perspective.metadata.container.topLevelTabs.includes = [{id: "camel"},{id: "connect"}];
+Perspective.metadata.container.topLevelTabs.includes = [{id: "camel"},{id: "connect"},{id: "dashboard"}];
 Perspective.metadata.container.label = "Recent";
 delete Perspective.metadata.container.icon;
